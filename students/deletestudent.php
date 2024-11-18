c536c991cafa5f980f4f3b0652d18f9c97402892
ebentarigan <ebenezer0852@gmail.com>
2024-11-18 08:51:50 +0700

    apa ini cok

--- students/deletestudent.php
+++ students/deletestudent.php
@@ -5,7 +5,13 @@ $nim = $_GET["nim"];
 
 $sqlStatement = "DELETE FROM students WHERE nim='$nim'";
 $query = mysqli_query($conn, $sqlStatement);
+$row =mysqli_fetch_assoc($query);
+
+$sqlStatement = "DELETE FROM students WHERE nim='$nim'";
+$query = mysqli_query($conn, $sqlStatement);
+
 if ($query) {
+    unlink("../images/".$row['foto']);
     $succesMsg = "Penghapusan data mahasiswa dengan NIM " . $nim . " berhasil";
     header("location:index.php?successMsg=$succesMsg");
 }
